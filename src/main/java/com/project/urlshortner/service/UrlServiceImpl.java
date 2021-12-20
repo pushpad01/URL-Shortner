4d409fd200c3122c53612e0e2c52c5066f46302c
pushpad01 <cse.pushpad1@gmail.com>
2021-12-20 17:55:12 +0530

    Code changes

--- src/main/java/com/project/urlshortner/service/UrlServiceImpl.java
+++ src/main/java/com/project/urlshortner/service/UrlServiceImpl.java
@@ -3,19 +3,45 @@ package com.project.urlshortner.service;
 import com.google.common.hash.Hashing;
 import com.project.urlshortner.model.Url;
 import com.project.urlshortner.model.UrlDto;
+import com.project.urlshortner.repository.UrlRepository;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
+@Component
 public class UrlServiceImpl implements UrlService {
+
+    @Autowired
+    private UrlRepository urlRepository;
 
     @Override
     public Url generateShortLink(UrlDto urlDto) {
         if (StringUtils.isNotEmpty(urlDto.getUrl())) {
             String encodedUrl = encodedUrl(urlDto.getUrl());
+            Url urlToPersist = new Url();
+            urlToPersist.setCreationDate(LocalDateTime.now());
+            urlToPersist.setOriginalUrl(urlDto.getUrl());
+            urlToPersist.setShortLink(encodedUrl);
+            urlToPersist.setExpiryDate(getExpirationDate(urlDto.getExpirationDate(), urlToPersist.getCreationDate()));
+            Url urlToRet = persistShortLink(urlToPersist);
+            if (urlToRet != null) {
+                return urlToRet;
+            } else {
+                return null;
+            }
         }
         return null;
+    }
+
+    private LocalDateTime getExpirationDate(String expirationDate, LocalDateTime creationDate) {
+        if (StringUtils.isBlank(expirationDate)) {
+            return creationDate.plusSeconds(120);
+        }
+        LocalDateTime expirationDateToRet = LocalDateTime.parse(expirationDate);
+        return expirationDateToRet;
     }
 
     private String encodedUrl(String url) {
@@ -27,16 +53,18 @@ public class UrlServiceImpl implements UrlService {
 
     @Override
     public Url persistShortLink(Url url) {
-        return null;
+        Url urlToRet = urlRepository.save(url);
+        return urlToRet;
     }
 
     @Override
     public Url getEncodedUrl(String url) {
-        return null;
+        Url urlToRet = urlRepository.findByShortLink(url);
+        return urlToRet;
     }
 
     @Override
     public void deleteShortLink(Url url) {
-
+        urlRepository.delete(url);
     }
 }
